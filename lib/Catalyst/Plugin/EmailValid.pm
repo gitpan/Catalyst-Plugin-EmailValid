9c82ea982d212dbef6a95772cade00b43a9fd8cd
Franck Cuny <franck+cpan@lumberjaph.net>
2005-08-20 12:06:30 +0000

    Import of FRANCKC/Catalyst-Plugin-EmailValid-0.01 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-EmailValid
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         FRANCKC/Catalyst-Plugin-EmailValid-0.01.tar.gz
    gitpan-cpan-author:       FRANCKC
    gitpan-cpan-maturity:     released

---
+++ lib/Catalyst/Plugin/EmailValid.pm
@@ -0,0 +1,64 @@
+package Catalyst::Plugin::EmailValid;
+
+use warnings;
+use strict;
+
+use Catalyst::Request;
+use Email::Valid;
+
+our $VERSION = 0.01;
+
+=head1 NAME
+
+Catalyst::Plugin::EmailValid - Email::Valid for Catalyst
+
+=head1 SYNOPSIS
+
+    use Catalyst /EmailValid/;
+    ...
+    $c->check_email();
+    
+=head1 DESCRIPTION
+
+This module determines whether an email address is well-formed.
+
+=cut
+
+=head2 METHOD
+
+=head3 check_email
+
+=cut
+
+sub check_email {
+    my $c = shift;
+    my $params;
+    if ($_[0]) {
+        $params = $_[1] ? {@_} : $_[0];
+    }
+    $c->log->debug('Checking Mail "'.$c->req->params->{email}.'"');
+    unless (Email::Valid->address($params ? %$params : $c->req->params->{email})) {
+        $c->log->debug('F
+        ailed "'.$Email::Valid::Details.'"');
+        $c->stash->{error}   = 1;
+        $c->stash->{message} = "Email not valid.";
+    }
+    return $c;
+}
+
+=head1 SEE ALSO
+
+L<Catalyst>, L<Email::Valid>
+
+=head1 AUTHOR
+
+Franck Cuny <franck@breizhdev.net>
+
+=head1 COPYRIGHT
+
+This program is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
+1;
